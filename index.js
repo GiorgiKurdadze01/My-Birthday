5f9251c75a934195daad4509aeff094dcf1a04c1
GiorgiKurdadze01 <giorgikurdadze01@gmail.com>
2023-12-07 14:02:57 +0400

    my birthday

---
+++ index.js
@@ -0,0 +1,50 @@
+var currentdate = new Date();
+const cardEl = document.querySelector('.hb-card')
+const wrongEl = document.querySelector('.hb-wrongTime')
+const audio = document.querySelector('.hb-audio')
+const cameraEl = document.querySelector('.hb-story')
+const buttonEl = document.querySelector('.hb-button')
+const inputEl = document.querySelector('.hb-inputName')
+let day = currentdate.getDate()
+let month = currentdate.getMonth() + 1
+let year = currentdate.getFullYear()
+let hours = currentdate.getHours()
+let minutes = currentdate.getMinutes()
+let nameEl = false
+
+function congrats() {
+    getValue()
+    showHbdCard()
+}
+function getValue() {
+    let val = document.querySelector('.hb-inputName').value
+    let hbNameEl = document.querySelector('.hb-name')
+    hbNameEl.innerHTML = val
+    if (val != false) {
+        nameEl = true;
+    }
+}
+function showHbdCard() {
+    if (day === 8 && month === 12) {
+        if (nameEl == true) {
+            cardEl.classList.add('hb-open')
+            cardEl.classList.remove('hb-card')
+            audio.play();
+            cameraEl.style.display = 'none'
+            buttonEl.style.display = 'none'
+            inputEl.style.display = 'none'
+            wrongEl.style.display = 'none'
+        } else {
+            wrongEl.innerHTML = 'Pleas input your name first'
+            wrongEl.style.display = 'flex'
+        }
+
+    } else {
+        wrongEl.innerHTML = "It's Too early please wait 8 December"
+        wrongEl.style.display = 'flex'
+    }
+
+}
+
+
+
